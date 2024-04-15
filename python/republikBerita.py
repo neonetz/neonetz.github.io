bd8dbd57756eca5a5da2ec73490ad0394773f4cd
Muhammad Adhyaksa Fadillah <aksafadillah@gmail.com>
2024-04-15 18:52:44 +0700

    Update data

--- python/republikBerita.py
+++ python/republikBerita.py
@@ -3,6 +3,23 @@ import requests
 import json
 import re
 from datetime import datetime
+import subprocess
+
+# Mendefinisikan fungsi untuk melakukan commit dan push perubahan ke GitHub
+def commit_and_push_changes():
+    try:
+        # Perintah Git untuk menambahkan semua perubahan
+        subprocess.run(["git", "add", "."], check=True)
+        # Perintah Git untuk melakukan commit dengan pesan "Update data"
+        subprocess.run(["git", "commit", "-m", "Update data"], check=True)
+        # Perintah Git untuk melakukan push ke branch saat ini
+        subprocess.run(["git", "push"], check=True)
+        print("Perubahan berhasil di-commit dan di-push ke GitHub")
+    except subprocess.CalledProcessError as e:
+        # Menangani kesalahan jika terjadi
+        print("Terjadi kesalahan saat melakukan commit dan push perubahan:", e)
+
+
 
 html = requests.get('https://news.republika.co.id/')
 soup = BeautifulSoup(html.text,'html.parser')
@@ -41,3 +58,6 @@ with open('berita.json', 'w') as f:
 
 with open('berita_special.json', 'w') as f:
     json.dump(infoNewsSpecial, f)
+
+# Memanggil fungsi commit_and_push_changes setelah menulis data ke file JSON
+commit_and_push_changes()
